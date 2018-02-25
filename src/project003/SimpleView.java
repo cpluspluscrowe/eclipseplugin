db3cb67e80e9d04245fce226612f4e4cdc8cc671
Chad Crowe <ccrowe@linkedin.com>
2018-02-25 07:51:32 -0800

    this one works

---
+++ src/project003/SimpleView.java
@@ -0,0 +1,35 @@
+package project003;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.e4.ui.services.EMenuService;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+
+public class SimpleView {
+	private static final String POPUPMENU_ID = "project003.popupmenu.mypopupmenu";
+	public static final String VIEW_ID = "project003.partdescriptor.simpleview1example";
+	private StyledText styledText;
+	
+	@Inject
+	public SimpleView() {}
+	
+	@PostConstruct
+	public void postConstruct(Composite parent, EMenuService menuService) {
+		Composite container = new Composite(parent, SWT.NONE);
+		container.setLayout(new FillLayout(SWT.HORIZONTAL));
+		styledText = new StyledText(container, SWT.BORDER);
+		menuService.registerContextMenu(styledText, POPUPMENU_ID);
+	}
+
+	public void appendText(String str) {
+		this.styledText .append(str);
+	}
+	public void setText(String str) {
+		this.styledText.setText(str);
+	}
+
+}
